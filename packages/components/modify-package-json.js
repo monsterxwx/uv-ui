01d21215e8c359b86bde2b41215f60afe98017cb
xwx623989195 <623989195@qq.com>
2023-04-03 14:08:05 +0800

    docs: 更新版本

--- packages/components/modify-package-json.js
+++ packages/components/modify-package-json.js
@@ -1,11 +1,11 @@
-const fs= require('fs')
-const path =require('path')
+const fs = require('fs')
+const path = require('path')
 
-const packageJsonPath =path.join(__dirname,'package.json')
-const packageJsonData= JSON.parse(fs.readFileSync(packageJsonPath))
+const packageJsonPath = path.join(__dirname, 'package.json')
+const packageJsonData = JSON.parse(fs.readFileSync(packageJsonPath))
 
-packageJsonData.main ='dist/es/index.js'
-packageJsonData.module ='dist/es/index.js'
-packageJsonData.style ='dist/es/style.css'
+packageJsonData.main = 'dist/es/index.js'
+packageJsonData.module = 'dist/es/index.js'
+packageJsonData.style = 'dist/es/style.css'
 
-fs.writeFileSync(packageJsonPath,JSON.stringify(packageJsonData,null,2))
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonData, null, 2))
