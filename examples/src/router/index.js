6291d802bdb90bd3a44e2e83d7a4646e8afeb9ee
xwx623989195 <623989195@qq.com>
2023-05-12 16:14:31 +0800

    docs: example打包更新

--- examples/src/router/index.js
+++ examples/src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routerList from './routerList'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: routerList
 })
 
