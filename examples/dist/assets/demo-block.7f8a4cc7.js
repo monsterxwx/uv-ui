11761f4323469c8d91aa63ab55dac731493f6ab5
xwx623989195 <623989195@qq.com>
2023-05-15 16:58:46 +0800

    docs: example打包

--- examples/dist/assets/demo-block.7f8a4cc7.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c,d,g as e,t as _,l}from"./index.fb11df31.js";const a={class:"demo-block"},n={class:"demo-block-title"},r={__name:"demo-block",props:{title:String},setup(t){return(o,i)=>(c(),d("div",a,[e("div",n,_(t.title),1),e("div",null,[l(o.$slots,"default",{},void 0,!0)])]))}},m=s(r,[["__scopeId","data-v-4bf1d9e4"]]);export{m as d};
