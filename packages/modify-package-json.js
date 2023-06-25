2eeb3dab2c74002cea4df47c656021a9daf1d156
xwx623989195 <623989195@qq.com>
2023-06-25 14:01:00 +0800

    docs(package): 引入semver，修改组件打包配置

--- packages/modify-package-json.js
+++ packages/modify-package-json.js
@@ -1,10 +1,15 @@
 const fs = require('fs')
 const path = require('path')
+const semver = require('semver')
 
 const packageJsonPath = path.join(__dirname, 'package.json')
 const packageJsonData = JSON.parse(fs.readFileSync(packageJsonPath))
 
+// 配置组件打包后的主文件地址
 packageJsonData.main = 'lib/packages/components/index.cjs'
 packageJsonData.module = 'es/packages/components/index.js'
+
+// 增加版本号
+packageJsonData.version = semver.inc(packageJsonData.version, 'patch')
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonData, null, 2))
